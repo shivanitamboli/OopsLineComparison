bde2d8e24d3265de87dc2ea4e2700b07d9ddcf48
[shivani tamboli] <[tambolishivani4@gmail.com]>
2021-09-27 18:29:55 +0530

    Added : length of line

---
+++ src/com/bridgelab/LineComparison.java
@@ -0,0 +1,17 @@
+package com.bridgelab;
+
+public class LineComparison {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome To Line Comparison Computation Program ");
+        int x1 = 5;
+        int x2 = 7;
+        int y1 = 8;
+        int y2 = 89;
+
+        double lengthOfLine1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        System.out.println("Length of Line : "+lengthOfLine1);
+    }
+	}
+
+
